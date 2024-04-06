ab3fb93cfad84370a63d2ca2766051609ee3d2f9
martyna.radz <mar.rad97@gmail.com>
2024-04-06 09:21:55 +0200

    zad1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Point point = new Point(1,1);
+
+        System.out.println(point.toString());
     }
 }
